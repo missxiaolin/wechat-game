fa6365f2be1800b678baf5a05a24c70064f60dd1
xiaolin <462441355@qq.com>
2018-03-10 19:11:01 +0800

    * 铅笔绘制

--- Main.js
+++ Main.js
@@ -6,24 +6,29 @@ import { Land } from "./js/runtime/Land.js";
 
 // 初始化整个游戏的精灵，作为游戏入口
 export class Main {
-    constructor () {
+    constructor() {
         this.canvas = document.getElementById('game_canvas')
         this.ctx = this.canvas.getContext('2d')
         // 变量缓存
         this.dataStore = DataStore.getInstance()
+        this.director = Director.getInstance()
         const loader = ResourceLoader.create()
         loader.onLoaded(map => this.onResourceFirstLoaded(map))
     }
 
-    onResourceFirstLoaded (map) {
+    onResourceFirstLoaded(map) {
         this.dataStore.ctx = this.ctx
         this.dataStore.res = map
         this.init()
     }
 
     init() {
-        this.dataStore.put('background', BackGroud)
-                        .put('land',Land)
-        Director.getInstance().run()
+        this.dataStore.put('pencils', [])
+            .put('background', BackGroud)
+            .put('land', Land)
+
+        // 要在游戏运行之前先创建铅笔
+        this.director.createPencil()
+        this.director.run()
     }
- }
+}
--- js/Director.js
+++ js/Director.js
@@ -1,22 +1,52 @@
 import { DataStore } from "./base/DataStore.js";
+import { UpPencil } from "./runtime/UpPencil.js";
+import { DownPencil } from "./runtime/DownPencil.js";
 
 // 导演类，控制游戏逻辑
 export class Director {
     constructor() {
         this.dataStore = DataStore.getInstance()
+        this.moveSpeed = 2
     }
 
     static getInstance() {
         if (!Director.instance) {
-            Director.instance = new Director();
+            Director.instance = new Director()
         }
         return Director.instance;
+    }
+
+    createPencil() {
+        const minTop = window.innerHeight / 8
+        const maxTop = window.innerHeight / 2
+        const top = minTop + Math.random() * (maxTop - minTop)
+        this.dataStore.get('pencils').push(new UpPencil(top))
+        this.dataStore.get('pencils').push(new DownPencil(top))
     }
 
     // 运行
     run() {
         this.dataStore.get('background').draw()
+        // 铅笔
+        const pencils = this.dataStore.get('pencils')
+
+        if (pencils[0].x + pencils[0].width <= 0 &&
+            pencils.length === 4) {
+            pencils.shift();
+            pencils.shift();
+        }
+
+        if (pencils[0].x <= (window.innerWidth - pencils[0].width) / 2 &&
+            pencils.length === 2) {
+            this.createPencil();
+        }
+
+        pencils.forEach(function (value, index, array) {
+            value.draw()
+        });
+        // 地板
         this.dataStore.get('land').draw()
+
         let timer = requestAnimationFrame(() => this.run())
         this.dataStore.put('timer', timer)
         // 停止地板移动
